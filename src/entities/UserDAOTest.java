eda2abb75262e9cb93ea2e532e7ffc7470809dde
KUANAIT <230703@astanait.edu.kz>
2024-02-16 23:57:38 +0600

    choiceoperation

--- src/entities/UserDAOTest.java
+++ src/entities/UserDAOTest.java
@@ -49,6 +49,52 @@ public class UserDAOTest {
             System.out.println("Retrieved user is null");
         }
     }
+    public void testUpdate() throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter user ID to update:");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Enter new user details:");
+        System.out.print("Name: ");
+        String name = scanner.nextLine();
+        System.out.print("Surname: ");
+        String surname = scanner.nextLine();
+        System.out.print("Group Number: ");
+        int groupNumber = scanner.nextInt();
+        System.out.print("Attendance: ");
+        double attendance = scanner.nextDouble();
+
+        User user = new User(id, name, surname, groupNumber, attendance);
+
+        userDao.update(user);
+        System.out.println("Updated user: " + user);
+    }
+
+    public void testDelete() throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter user ID to delete:");
+        int id = scanner.nextInt();
+
+        userDao.delete(id);
+        System.out.println("Deleted user with ID: " + id);
+    }
+
+    public void testFind() throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter user ID to find:");
+        int id = scanner.nextInt();
+
+        User user = userDao.find(id);
+        if (user != null) {
+            System.out.println("Found user: " + user);
+        } else {
+            System.out.println("No user found with ID: " + id);
+        }
+    }
 
     public void closeConnection() throws SQLException {
         if (conn != null) {
